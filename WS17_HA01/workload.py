74e9391db4103027341124a64afcab151a886089
Jan Wirth <jan.wirth@cinteo.com>
2017-10-29 14:05:51 +0100

    Complete Ex 1 Part 2

--- WS17_HA01/workload.py
+++ WS17_HA01/workload.py
@@ -1,4 +1,6 @@
 #!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
 
 """ Calculate weekly workload for Prog1 course """
 __author__      = "Jan Wirth <contact@jan-wirth.de"
@@ -10,3 +12,9 @@ workload = lps * hours_per_lp
 weeks = 13
 workload_per_week = workload / weeks
 print(workload_per_week)
+
+
+print("Ein Leistungspunkt entspricht", hours_per_lp, "Stunden Arbeit.")
+print("Für den Kurs erhält man", lps, "Leistungspunkte.")
+print("Also: man soll für den Kurs insgesamt", workload, "Stunden lernen.")
+print("Dies entspricht einer wöchentlichen Lernzeit von", str(workload_per_week) + ".") # concat to remove space before full stop.
