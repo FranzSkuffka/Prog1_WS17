61489575f2dfbf982fa260605d2901f1cce0d031
Jan Wirth <jan.wirth@cinteo.com>
2017-11-15 12:53:18 +0000

    WIP - 2.2

--- WS17_HA02/immutables.py
+++ WS17_HA02/immutables.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+""" Examine immutable data types """
+__author__      = "Jan Wirth <contact@jan-wirth.de"
+__version__     = "0.0.1"
+
+
 """Übung zu veränderbaren und unveränderbaren Objekten.
 
 Hauptfrage: Wie werden veränderbare und unveränderbare Objekte geändert?
@@ -8,75 +13,133 @@ Hauptfrage: Wie werden veränderbare und unveränderbare Objekte geändert?
 
 """Kopie aus dem Python-Interpreter
 Vorübung: Führen Sie diese oder ähnliche Anweisungen durch, und vollziehen Sie die Rückgabewerte nach.
+"""
+"""
+i = 42
+id(i)
+# 4297638208
+i = list(range(3))
+i
+# [0, 1, 2]
+id(i)
+# 4339799816
+j = "ABBA"
+id(j)
+# 4339792560
+j = j+"C"
+j
+# 'ABBAC'
+id(j)
+# 4339792504
+k = 4
+id(k)
+# 4297636992
 
->>> i = 42
->>> id(i)
-4297638208
->>> i = list(range(3))
->>> i
-[0, 1, 2]
->>> id(i)
-4339799816
->>>
->>> j = "ABBA"
->>> id(j)
-4339792560
->>> j = j+"C"
->>> j
-'ABBAC'
->>> id(j)
-4339792504
->>>
->>> k = 4
->>> id(k)
-4297636992
->>>
->>> t = (i, j, k)
->>> t
-([0, 1, 2], 'ABBAC', 4)
->>> id(t)
-4339803336
->>>
->>> i.append(3)
->>> i
-[0, 1, 2, 3]
->>> id(i)
-4339799816
->>> t
-([0, 1, 2, 3], 'ABBAC', 4)
->>> id(t)
-4339803336
->>> j += "AT"
->>> t
-([0, 1, 2, 3], 'ABBAC', 4)
->>> id(t)
-4339803336
->>> k = 5
->>> t
-([0, 1, 2, 3], 'ABBAC', 4)
->>> id(k)
-4297637024
+t = (i, j, k)
+t
+# ([0, 1, 2], 'ABBAC', 4)
+id(t)
+# 4339803336
 
+i.append(3)
+i
+# [0, 1, 2, 3]
+id(i)
+# 4339799816
+t
+# ([0, 1, 2, 3], 'ABBAC', 4)
+id(t)
+# 4339803336
+j += "AT"
+t
+# ([0, 1, 2, 3], 'ABBAC', 4)
+id(t)
+# 4339803336
+k = 5
+t
+# ([0, 1, 2, 3], 'ABBAC', 4)
+id(k)
+# 4297637024
 """
 
 
 def check_immutables():
     """ Anweisungssequenz zum Üben der Änderung einiger unveränderbarer Datentypen """
-    
-    i = 42              #1
-    i = list(range(3))  #2
-    j = "ABBA"          #3
-    j = j + "C"         #4
-    k = 4               #5
-    t = (i, j, k)       #6
-    i.append(3)         #7 append() ist eine Listenfunktion, und hängt ein weiteres Element an die Liste
+    objects = set()
+    variableNames = set()
+
+    #1
+    # print('i Before 1' + str(type(i)) + str(id(i)))
+    i = 42              
+    variableNames.add('i')
+    objects.add(id(i))
+    print('i After 1' + str(type(i)) + str(id(i)))
+
+    #2
+    # print('i Before 2' + str(type(i)) + str(id(i)))
+    i = list(range(3))  
+    variableNames.add('i')
+    objects.add(id(i))
+    print('i After 2' + str(type(i)) + str(id(i)))
+
+    #3
+    # print('j Before 3' + str(type(j)) + str(id(j)))
+    j = "ABBA"          
+    variableNames.add('j')
+    objects.add(id(j))
+    print('j After 3' + str(type(j)) + str(id(j)))
+
+    #4
+    # print('j Before 4' + str(type(j)) + str(id(j)))
+    j = j + "C"         
+    variableNames.add('j')
+    objects.add(id(j))
+    print('j After 4' + str(type(j)) + str(id(j)))
+
+    #5
+    # print('k Before 5' + str(type(k)) + str(id(k)))
+    k = 4               
+    variableNames.add('k')
+    objects.add(id(k))
+    print('k After 5' + str(type(k)) + str(id(k)))
+
+    #6
+    # print('t Before 6' + str(type(t)) + str(id(t)))
+    t = (i, j, k)       
+    variableNames.add('t')
+    objects.add(id(t))
+    print('t After 6' + str(type(t)) + str(id(t)))
+
+    #7 append() ist eine Listenfunktion, und hängt ein weiteres Element an die Liste
+    # print('i Before 7' + str(type(i)) + str(id(i)))
+    i.append(3)         
+    variableNames.add('i')
+    objects.add(id(i))
+    print('i After 7' + str(type(i)) + str(id(i)))
+
+    print('i', i)
+    print('j', j)
+    print('j', j)
+    print('k', k)
+    print('t', t)
+
+    print('')
+    print('OBJECTS')
+    print(objects)
+    print(len(objects))
+
+    print('')
+    print('VARIABLENAMES')
+    print(variableNames)
+    print(len(variableNames))
+
     
     """FRAGEN:
     
     - Wie viele neue Objekte und wie viele neue Variablennamen wurden in der Funktion insgesamt erzeugt? Anhand welcher Eigenschaft kann man die Anzahl der Objekte nachvollziehen?
-        ANTWORT: neue Objekte:
-                 neue Namen:
-                 Erzeugung neuer Objekte nachvollziehbar durch:
+        ANTWORT: neue Objekte: 6
+                 neue Namen: 4
+                 Erzeugung neuer Objekte nachvollziehbar durch: Ein set mit der ID des objekts nach jeder operation
     
     - Listen Sie die veränderbaren und unveränderbaren Datentypen auf, die in der Funktion für die Variablen verwendet wurden.
         ANTWORT: VERÄNDERBAR:
@@ -98,3 +161,4 @@ def check_immutables():
     
     """
 
+check_immutables()
