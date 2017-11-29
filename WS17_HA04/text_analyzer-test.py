6e7daff7216f046a82cce1e8846ebd6010874550
Jan Wirth <jan.wirth@cinteo.com>
2017-11-29 09:56:55 +0100

    Complete 4.4

--- WS17_HA04/text_analyzer-test.py
+++ WS17_HA04/text_analyzer-test.py
@@ -7,7 +7,22 @@ from text_analyzer import main
 
 class Test(TestCase):
 
-    @patch('text_analyzer._input', side_effect=['Hello pretty world', 's'])
+    @patch('text_analyzer._input', side_effect=['Hello, pretty world!', 's'])
+    @patch('text_analyzer._print', create=True)
+    def test_simple(self, _print, _input ):
+        main()
+        _input.assert_has_calls([
+          call('Please enter some text here: '),
+          call('Please choose (s)imple (h)euristic: ')
+        ])
+
+        _print.assert_has_calls([
+          call('The text contains 3 words:'),
+          call('3 word(s) with 6 letters'),
+          call('Average word length is 6.0.')
+        ])
+
+    @patch('text_analyzer._input', side_effect=['Hello, pretty world!', 'h'])
     @patch('text_analyzer._print', create=True)
     def test_simple(self, _print, _input ):
         main()
@@ -20,6 +35,20 @@ class Test(TestCase):
           call('The text contains 3 words:'),
           call('2 word(s) with 5 letters'),
           call('1 word(s) with 6 letters'),
+          call('Average word length is 5.333333333333333.')
+        ])
+
+    @patch('text_analyzer._input', side_effect=['Hello, pretty world!', ''])
+    @patch('text_analyzer._print', create=True)
+    def test_simple(self, _print, _input ):
+        main()
+        _input.assert_has_calls([
+          call('Please enter some text here: '),
+          call('Please choose (s)imple (h)euristic: ')
+        ])
+
+        _print.assert_has_calls([
+          call('Mode not supported: ""')
         ])
 
 if __name__ == '__main__':
--- WS17_HA04/text_analyzer.py
+++ WS17_HA04/text_analyzer.py
@@ -6,6 +6,8 @@ __author__      = "Jan Wirth <contact@jan-wirth.de"
 __version__     = "0.0.1"
 
 from itertools import groupby
+from functools import reduce
+import re
 
 def _input(msg):
   """
@@ -19,24 +21,34 @@ def _print(msg):
   """
   return print(msg)
 
-def simple (text):
-  tokens = text.split(' ')
+def analyze (tokens):
+  """ Analyzes a given string with regard to token lengths """
   _print('The text contains ' + str(len(tokens)) + ' words:'),
   groups = {key:list(group) for key, group in groupby(sorted(tokens, key=len), len)}
   for key in groups:
     _print(str(len(groups[key])) + ' word(s) with ' + str(key) + ' letters')
 
-def heuristic (text):
-  _print('loool')
+  lengths = list(map(len, tokens))
+  total_length = reduce(sum, lengths)
+  avg_len = total_length / len(tokens)
+  _print('Average word length is ' + str(avg_len) + '.')
+
+def preprocess (token):
+  return re.sub('[^a-zA-Z]', '', token)
+
+""" a fp helper for reduce """
+def sum (a = 0, b = 0): return a + b
 
 def interactive_analysis():
-  modes = {
-    's': simple,
-    'h': heuristic
-  }
   text = _input('Please enter some text here: ')
   mode = _input('Please choose (s)imple (h)euristic: ')
-  modes[mode](text)
+  tokens = text.split(' ')
+  if mode == 's':
+    analyze(tokens)
+  elif mode == 'h':
+    analyze(list(map(preprocess, tokens)))
+  else:
+    _print('Mode not supported: "' + mode + '"')
 
 
 
