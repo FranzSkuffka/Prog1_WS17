2b08a0aa5ef5d3cf464ea67171011c9b5fc72553
Jan Wirth <jottenwe@gmail.com>
2017-12-15 11:16:44 +0100

    "Improve" exception exercise

--- WS17_HA06/exceptions_emm.py
+++ WS17_HA06/exceptions_emm.py
@@ -1,93 +1,91 @@
 #!/usr/bin/env python3
 
 """Übung zur Erzeugung und Behandlung von Fehlern
-
 Autor: Jan Wirth
-Version: 20171208
 """
 
 import sys
 
-def handle_ModuleNotFoundError():
+def provoke_and_handle_ModuleNotFoundError():
     """Handling for module not found errors. Tries to import a module that doesn't exist."""
     try:
-        import aiujsdaiosjdiajhsd
+        import arsiton324
     except ModuleNotFoundError as mnfe:
         print(f"Sorry! {mnfe} was found!")
 
-def handle_KeyError():
+def provoke_and_handle_KeyError():
     """Handling for key errors. Tries looking for a key which doesn't exist in given dictionary."""
     test_dict = {}
     try:
-        print(test_dict[15])
+        print(test_dict['to life'])
     except KeyError as ke:
-        print(f"Sorry! The key {ke} does not exist in test_dict!")
+        print(f"Sorry! The key '{ke}' does not exist in test_dict!")
 
-def handle_NameError():
+def provoke_and_handle_NameError():
     """Handling for name errors. Tries to print a variable which hasn't been defined."""
     try:
         print(bliblablub)
     except NameError as ne:
         print(f"Sorry! {ne}")
 
-def handle_ZeroDivisionError():
+def provoke_and_handle_ZeroDivisionError():
     """Handling for zero divison errors. Tries to divide 3 by 0."""
     try:
-        print(3/0)
+        print(324/0)
     except ZeroDivisionError as zde:
         print(f"Sorry! {zde}")
 
-def handle_SyntaxError():
+def provoke_and_handle_SyntaxError():
     """Handling for syntax errors. Tries to call the eval function on a string."""
     try:
         eval("x === x")
     except SyntaxError as se:
         print(f"Sorry! You can't use eval in that way: {se}")
 
-def handle_TypeError():
+def provoke_and_handle_TypeError():
     """Handling for type errors. Tries to add and int to a string."""
     try:
-        print("Hihi" + 3)
+        print("loetungdusohn" + 3)
     except TypeError as te:
         print(f"Sorry! {te}")
 
-def handle_ImportError():
+def provoke_and_handle_ImportError():
     """Handling for import errors. Tries to import from a module which doesn't exist."""
     try:
-        from plup import plap
+        from you import me
     except ImportError as impe:
         print(f"Sorry! {impe}")
 
-def handle_FileNotFoundError():
+def provoke_and_handle_FileNotFoundError():
     """Handling for file not found errors. Tries to open a file which doesn't exist."""
     try:
-        with open("aiusdiuahsdiuhasiudh.txt") as f:
+        with open("NEIN.mp3") as f:
             print("well")
     except FileNotFoundError as fnfe:
         print(f"Sorry! {fnfe}")
 
-def handle_OverflowError():
+def provoke_and_handle_OverflowError():
     """Handling for overflow errors. Tries to exponate two massive numbers."""
     try:
         7771237.123**12837981723987
     except OverflowError as oe:
-        print(f"Sorry, overflow error occurred! {oe}")
+        print(f"Oopsie, overflow error occurred! {oe}")
 
-def handle_SystemExitError():
+def provoke_and_handle_SystemExitError():
     """Handling for system exit errors. Exits the script and gives a report on such action."""
     try:
         sys.exit()
     except SystemExit:
-        print(f"Ok, bye, you exited the script!")
+        print(f"I don't want to see you anymore!!")
 
 
 
 def error_handlings():
-    """Aufruf von allen Funktionen im Skript, deren Namen mit 'handle_' anfängt."""
+    """Call all functions that start with 'provoke_and_handle_'."""
 
     global_names = globals()
     for name in global_names:
-        if name.startswith("handle_"):
+        if name.startswith("provoke_and_handle_"):
             print("\nAUFRUF von '{}':".format(name))
             global_names[name]()
 
