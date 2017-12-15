1ddcdc8c1058b670cd46b24f691f22fe50a508e9
Jan Wirth <jottenwe@gmail.com>
2017-12-15 11:11:54 +0100

    Complete ngram reader

--- WS17_HA06/conll_reader.py
+++ WS17_HA06/conll_reader.py
@@ -4,29 +4,31 @@
 # Version: 0.0.1
 # Author: Jan Wirth
 # Description: Extract ngram sequences from tokenized file
-# Last modified: 2017-12-12T18:02:19.239Z
 # -----------------------------------
 
 import sys
 
+def find_ngrams(input_list, n):
+  return zip(*[input_list[i:] for i in range(n)])
 
 def conll_reader(filename, n, output_file):
+    print(filename)
     """This function extracts tokens from a given file, picks out n-long sequences of ngrams and sorts them into a new list. Items of that list are written into a new file and separated by linebreaks."""
 
     with open(filename) as active_file:
         lines = active_file.readlines()
         word_list = [line.split()[3] for line in lines if line.strip()
                      and not line.startswith("#")]
-        extract_ngrams = [
-            word_list[i:i + int(n)] for i in range(len(word_list) - int(n) + 1)]
+        ngrams = find_ngrams(word_list, n)
 
     with open(output_file, "w") as output:
-        for item in sorted(extract_ngrams):
-            output.write(str(item))
-            output.write("\n")
+        for ngram in sorted(map( lambda ngram: ' '.join(ngram), ngrams)):
+            if type(ngram == "<class 'str'>"):
+              output.write(str(ngram))
+              output.write("\n")
 
     return
 
 
 if __name__ == "__main__":
-    print(conll_reader(sys.argv[1], sys.argv[2], sys.argv[3]))
+    conll_reader(sys.argv[1], int(sys.argv[2]), sys.argv[3])
