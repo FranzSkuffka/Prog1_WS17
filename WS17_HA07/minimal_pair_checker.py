b3cbb4555a8ded484c519a850fae0ca50a6f4876
Jan Wirth <jottenwe@gmail.com>
2017-12-22 09:20:38 +0100

    Complete equal length algorithm

--- WS17_HA07/minimal_pair_checker.py
+++ WS17_HA07/minimal_pair_checker.py
@@ -57,7 +57,47 @@ def read(path):
 
       return extract_txt_lines('minimal', minimal_txt) + extract_txt_lines('notminimal', non_minimal_txt)
 
+def compare (pair):
+  type = pair[0]
+  a = pair[1][0]
+  b = pair[1][1]
+  if len(a) == len(b):
+    mismatch_count = 0
+    for i in range(len(a)):
+      if a[i] != b[i]:
+        mismatch_count = mismatch_count + 1
+    if mismatch_count == 1 and type == 'minimal': # TRUE POSITIVE
+      return (type, pair[1], 'TP')
+    elif type == 'minimal': # FALSE NEGATIVE
+      return (type, pair[1], 'FN')
+    elif mismatch_count == 1 and type == 'notminimal': # FALSE POSITIVE
+      return (type, pair[1], 'FP')
+    elif type == 'notminimal': # TRUE NEGATIVE
+      return (type, pair[1], 'TN')
+
+
+def levenshtein(s1, s2):
+    if len(s1) < len(s2):
+        return levenshtein(s2, s1)
+
+    # len(s1) >= len(s2)
+    if len(s2) == 0:
+        return len(s1)
+
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1 # j+1 instead of j since previous_row and current_row are one character longer
+            deletions = current_row[j] + 1       # than s2
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+    
+    return previous_row[-1]
+
 if __name__ == "__main__":
   print("\nstarting minimal pair checker")
   pairs = read(sys.argv[1])
   print(pairs)
+  print(list(map(compare, pairs)))
